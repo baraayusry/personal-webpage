a1e1ceab3b420a568274deaf142acdda73f48f50
Elbaraa Elsaadany <elbaraa.elsaadany@student.uibk.ac.at>
2025-09-13 00:20:09 +0200

    update log url

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -5,7 +5,7 @@ export const experience: ExperienceItem[] = [
 title: 'Software Developer Intern',
 org: 'Onlim GmbH',
 location: 'Innsbruck , Austria ',
-logo: './src/assets/onlim_logo.png',
+logo: 'https://status-premium.onlim.com/media/statuspages/onlim_logo_CaIsRBN.png',
 start: '2023 (Jan)',
 end: '2023 (Jun)',
 bullets: ['Dialog engine development', 'Improving chatbot functionalities','Integrating Dailog Engine with Knowledge Graphs Services']
@@ -15,7 +15,7 @@ bullets: ['Dialog engine development', 'Improving chatbot functionalities','Inte
 title: 'Software Enigneer',
 org: 'Lagorta',
 location: 'Dubai , UAE ',
-logo: './src/assets/lagorta_logo.jpeg',
+logo: 'https://media.licdn.com/dms/image/v2/C560BAQGL2-lUfOQVHQ/company-logo_200_200/company-logo_200_200/0/1630590521309/lagorta_logo?e=2147483647&v=beta&t=E6mk7vBTh6Kg4IS6dx_JUtS3Fx-FrRL9WBDESrb32VA',
 start: '2021',
 end: '2022',
 bullets: ['Developing and maintain Java backend services', 'microservices architecture for marketing functionalities.','unit and integration testing for code Quality assurance.']
@@ -24,7 +24,7 @@ bullets: ['Developing and maintain Java backend services', 'microservices archit
 title: 'Front-End Developer Intern',
 org: 'Ata Bilisim',
 location: 'Ankara , Turkey ',
-logo: './src/assets/ata-bilisim-logo.png',
+logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPARWlZGccDpxjRiVY3-cfc6uedjAqXTUIIA&s',
 start: '2021(Jul)',
 end: '2022(Oct)',
 bullets: ['implementing UI designs', 'optimizing website performance.','optimizing SEO.']
